ba25d13641563654236a346d84a7a7a7f48cac6f
Lucia Manzorro Castrillon <lucia_vejer@hotmail.com>
2016-12-21 20:24:13 +0100

    funcion1 version 3

--- funcion1.py
+++ funcion1.py
@@ -7,4 +7,4 @@ Created on Wed Dec 21 20:05:40 2016
 
 def funcion1():
     print('Primera funcion de mi projecttwo')
-    print('Modifico la primera funcion')
+    print('Modifico la primera funcion 2')
